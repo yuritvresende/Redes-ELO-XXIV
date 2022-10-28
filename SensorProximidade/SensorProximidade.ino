54fb7ba16d93a6e0832070e1ecf60487e4c6474b
Yuri Resende <yuritvresende@gmail.com>
2022-10-28 10:26:38 -0300

    Padronização dos Arquivos

--- SensorProximidade/SensorProximidade.ino
+++ SensorProximidade/SensorProximidade.ino
@@ -1,31 +1,24 @@
-/*
- * This ESP32 code is created by esp32io.com
- *
- * This ESP32 code is released in the public domain
- *
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-motion-sensor
- */
-
 const int PIN_TO_SENSOR = 33; // GIOP19 pin connected to OUTPUT pin of sensor
 int pinStateCurrent   = LOW;  // current state of pin
 int pinStatePrevious  = LOW;  // previous state of pin
 
-void setup() {
-  Serial.begin(115200);            // initialize serial
-  pinMode(PIN_TO_SENSOR, INPUT); // set ESP32 pin to input mode to read value from OUTPUT pin of sensor
+void setup() 
+{
+ Serial.begin(115200);          
+ pinMode(PIN_TO_SENSOR, INPUT);
 }
 
-void loop() {
-  pinStatePrevious = pinStateCurrent; // store old state
-  pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
+void loop() 
+{
+ pinStatePrevious = pinStateCurrent;
+ pinStateCurrent = digitalRead(PIN_TO_SENSOR);   
 
-  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
-    Serial.println("Motion detected!");
-    // TODO: turn on alarm, light or activate a device ... here
-  }
-  else
-  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {   // pin state change: HIGH -> LOW
-    Serial.println("Motion stopped!");
-    // TODO: turn off alarm, light or deactivate a device ... here
-  }
+ if (pinStatePrevious == LOW && pinStateCurrent == HIGH) 
+ {  
+  Serial.println("Motion detected!");
+ }
+ else if (pinStatePrevious == HIGH && pinStateCurrent == LOW) 
+ {   
+  Serial.println("Motion stopped!");
+ }
 }
