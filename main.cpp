e521decbf9770d3e47f8afefd12e2a488699311a
Azamor <52185981+azamor-luccas@users.noreply.github.com>
2022-11-11 10:08:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
-//site https://www.fernandok.com/2017/11/introducao-ao-esp32.html
-//#include "WiFi.h"
-//#include "./UD7/program.h"
-//#include "./Program7.h"
-
 #include "./trabEmGrupo/serverLDR.h"
-//#include "./trabEmGrupo/sensorLDR.h"
-
-//LDR
-
 
 void setup()
 {
